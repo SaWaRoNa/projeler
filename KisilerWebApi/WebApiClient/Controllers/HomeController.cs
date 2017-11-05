8528e657f02d6313221a6b3a3c24c96e4888df8f
Ramazan Arıcan <rmznasdas01@gmail.com>
2017-11-05 16:01:17 +0300

    KisilerWebApi

--- KisilerWebApi/WebApiClient/Controllers/HomeController.cs
+++ KisilerWebApi/WebApiClient/Controllers/HomeController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http.Cors;
 using System.Web.Mvc;
 
 namespace WebApiClient.Controllers
 {
     public class HomeController : Controller
     {
+        [EnableCorsAttribute("*", "*", "*")]
         public ActionResult Index()
         {
                        return View();
